0605527175f8a8526fde5691403d18b23806be80
Chase Zhang <yun.er.run@gmail.com>
2014-11-12 14:11:18 +0800

    add frontend app

---
+++ frontend/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^logout/', 'django.contrib.auth.views.logout_then_login', name='logout'),
+    url(r'^login/', 'django.contrib.auth.views.login', {'template_name':'login.html'}, name='login'),
+)
